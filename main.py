3eb73af114336c8be3e0f7ff24c12e3e5a69eada
saparboyevmagrur-blip <saparboyevmagrur.com>
2026-05-23 16:27:52 +0500

    telegram bot buttons

---
+++ main.py
@@ -0,0 +1,34 @@
+import telebot
+from telebot import types
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    keyboard = types.ReplyKeyboardMarkup()
+    btn1 = types.KeyboardButton("Men haqimda")
+    btn2 = types.KeyboardButton("Loyihalarim")
+    keyboard.add(btn1, btn2)
+    text = "Assalom alaykum, men Asadbek Rakhimov. \nBu mening portfolio botim. \nQuyidagi bo'limlardan birini tanlang"
+
+    bot.send_message(message.chat.id, text, reply_markup=keyboard)
+
+@bot.message_handler(func=lambda m: m.text == "Men haqimda")
+def aboutme_handler(message):
+    text = "Men Asadbek frontend engineermen. React.js & Next.js specialistman"
+    
+    bot.send_message(message.chat.id, text)
+
+@bot.message_handler(func=lambda m: m.text == "Loyihalarim")
+def projects_handler(message):
+    text = "Bu qism tez orada qo'shiladi"
+    
+    bot.send_message(message.chat.id, text)
+	
+bot.infinity_polling()
